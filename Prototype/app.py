eff7cc4aa2664bb90fa2e985ee95097e52f47ce2
AshTNA <ashleyaburnett@gmail.com>
2022-02-16 10:53:46 +0000

    Add export/share options

--- Prototype/app.py
+++ Prototype/app.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, request
 from forms.catalogue_search import CatalogueSearch
+from forms.featured_results import FeaturedResults
 import os, json
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route("/")
-def hello_world():
-    return "<p>Go to <a href='/search'>/search</a></p>"
+def index():
+    return """<p>
+    <a href='/search/catalogue'>Catalogue search</a>
+    <a href='/search/featured'>Featured results</a>
+    </p>"""
 
 @app.route("/search")
+def search():
+    return """<p>
+    <a href='/search/catalogue'>Catalogue search</a>
+    <a href='/search/featured'>Featured results</a>
+    </p>"""   
+
+@app.route("/search/catalogue")
 def catalogue_results():
     form = CatalogueSearch(request.args, meta = {'csrf': False})
-    return render_template("catalogue_results.html", form=form, search_type="Catalogue results", search_term="Churchill");
+    return render_template("catalogue_results.html", form=form, search_type="Catalogue results", search_term="Churchill", selected_tab={'catalogue': True});
+
+@app.route("/search/featured")
+def featured_results():
+    form = FeaturedResults(request.args, meta = {'csrf': False})
+    return render_template("featured_results.html", form=form, search_type="Catalogue results", search_term="Churchill", selected_tab={'featured': True});
 
---
+++ Prototype/forms/featured_results.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import SearchField
+from wtforms.validators import InputRequired
+
+
+class FeaturedResults(FlaskForm):
+
+    search_term = SearchField(
+        'Search term',
+        validators=[InputRequired()],
+        render_kw={
+            'class': 'search-results-hero__form-search-box',
+            'role': 'search',
+        }
+    )
