42730d50e605e6ddb1354b7a9041be07fae2e0c0
AshTNA <ashleyaburnett@gmail.com>
2022-03-04 12:14:22 +0000

    Ensure selected bucket always displays on mobile

--- Prototype/static/js/search-buckets-expander.js
+++ Prototype/static/js/search-buckets-expander.js
@@ -6,7 +6,7 @@ let searchBucketsExpander = function() {
         return;
     }
 
-    $searchBucketsToHide = document.querySelectorAll("ul[data-id=search-buckets-list] li:not(:first-child)");
+    $searchBucketsToHide = document.querySelectorAll("ul[data-id=search-buckets-list] li:not([data-current=True])");
 
     let $showHideButton = document.createElement('button');
     $showHideButton.innerText = 'Show more result categories';
@@ -79,36 +79,3 @@ let searchBucketsExpander = function() {
     }, 200));
 
 }();
-
-/**
- *
- * Invoke a given callback after debounce function
- * hasn't been called for a specified number of milliseconds
- *
- * usage:
- * debounce( call_back, 500 )( ..arg );
- *
- * @param {Function} call_back
- * @param {Number} wait
- * @param {Object} this_argument
- **/
-
- function debounce(call_back, wait, this_argument) {
-
-    var timer = null;
-
-    return function (...args) {
-
-        var context = this_argument || this;
-
-        window.clearTimeout(timer);
-
-        timer = window.setTimeout(() => {
-
-            timer = null;
-
-            call_back.apply(context, args);
-
-        }, wait);
-    };
-}
