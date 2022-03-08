9b6e1cd079c0b6de9b57d83ef56aad236618de71
AshTNA <ashleyaburnett@gmail.com>
2022-03-08 10:32:15 +0000

    Add mobile result explainer paragraphs
    This commit also makes selected buckets appear as spans instead of links.

--- Prototype/static/js/src/global-search.js
+++ Prototype/static/js/src/global-search.js
@@ -16,24 +16,23 @@ export default function () {
     $globalSearchComponent.hidden = true;
     $globalSearchComponent.id = 'gs-component';
     $globalSearchComponent.classList.add('global-search'); 
-    $globalSearchComponent.innerHTML = `
-
-        <div class="global-search__container">
-            <h1 class="global-search__heading">Search</h1>
-            <form class="global-search__form" action='/search/featured/' method='GET' id='global-search-form'>
-                <label for="search_term" class="global-search__label">
-                    <span class="sr-only">Enter search term.</span> For example, naturalisation or medal cards
-                </label>
-                <input type='search' class='global-search__form-search-box' id='search_term' >
-                <input type="submit" value="Search" class="global-search__form-submit">
-            </form>
-            <p class='global-search__paragraph'>Other ways to find things:</p>
-            <ul class='global-search__list'>
-                <li class='global-search__list-item'>Try the <a href='/search'>search page</a> for more options</li>
-                <li class='global-search__list-item'><a href=''>Explore the collection</a> through topics and time periods</li>
-                <li class='global-search__list-item'><a href=''>Discover Insights</a> for unique stories behind our collection.</li>
-            </ul>
-        </div>`;
+    $globalSearchComponent.innerHTML = 
+    `<div class="global-search__container">
+        <h1 class="global-search__heading">Search</h1>
+        <form class="global-search__form" action='/search/featured/' method='GET' id='global-search-form'>
+            <label for="search_term" class="global-search__label">
+                <span class="sr-only">Enter search term.</span> For example, naturalisation or medal cards
+            </label>
+            <input type='search' class='global-search__form-search-box' id='search_term' >
+            <input type="submit" value="Search" class="global-search__form-submit">
+        </form>
+        <p class='global-search__paragraph'>Other ways to find things:</p>
+        <ul class='global-search__list'>
+            <li class='global-search__list-item'>Try the <a href='/search'>search page</a> for more options</li>
+            <li class='global-search__list-item'><a href=''>Explore the collection</a> through topics and time periods</li>
+            <li class='global-search__list-item'><a href=''>Discover Insights</a> for unique stories behind our collection.</li>
+        </ul>
+    </div>`;
 
     $main.insertBefore($globalSearchComponent, $main.childNodes[0]); //IE11 compatible prepend
 
